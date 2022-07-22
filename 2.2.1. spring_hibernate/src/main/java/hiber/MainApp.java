af91f417b0dbcc172170bfbbf374eebbb885fed3
Khaizar1 <khaizar@mail.ru>
2022-07-22 16:34:19 +0300

    Дубль №1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,22 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Lada", 02)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Lada", 01)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Lada", 07)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Lada", 06)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
          System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println("First Name : "+user.getFirstName());
+         System.out.println("Last Name : "+user.getLastName());
+         System.out.println("Email : "+user.getEmail());
+         System.out.println("Car model : "+ user.getCar().getModel());
+         System.out.println("Car series : "+ user.getCar().getSeries());
+         System.out.println("______________________");
       }
+      System.out.println(userService.getUserByCar("Honda",50));
 
       context.close();
    }
